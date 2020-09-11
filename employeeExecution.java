d54b07094402bcc6286a66b59bb64a1d7a0d2f6e
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-09-11 10:10:24 +0530

    refactor code with function,adding main class

---
+++ employeeExecution.java
@@ -0,0 +1,8 @@
+public class employeeExecution
+{
+        public static void main(String args[])
+        {
+                refactorWithClassEmpWage obj = new refactorWithClassEmpWage();
+                obj.wageFunction();
+        }
+}
