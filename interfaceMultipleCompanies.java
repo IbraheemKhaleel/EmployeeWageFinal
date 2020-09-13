ef50c88326834829e30f708e01034d4e602fbcd8
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-09-13 22:46:46 +0530

    refactor code with interface

---
+++ interfaceMultipleCompanies.java
@@ -0,0 +1,5 @@
+public interface interfaceMultipleCompanies 
+{
+	public void addCompanyArray(String company, int wagePerHour, int totalWorkingTime, int totalWorkingDays) ;
+	public void computeEmpWage();
+}
